2e02a9b46c0522e58d5b487d27bd4b6c39bcdf1d
developervws004 <developervws#4>
2022-01-09 00:36:13 +0530

    Fragments Added

--- app/src/main/java/com/example/social_app/Dashboard.java
+++ app/src/main/java/com/example/social_app/Dashboard.java
@@ -1,14 +1,57 @@
 package com.example.social_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.social_app.databinding.ActivityDashboardBinding;
+import com.example.social_app.fragments.AddFragment;
+import com.example.social_app.fragments.FavtFragment;
+import com.example.social_app.fragments.Homefragment;
+import com.example.social_app.fragments.ProfileFragment;
+import com.example.social_app.fragments.SearchFragment;
+import com.iammert.library.readablebottombar.ReadableBottomBar;
 
 public class Dashboard extends AppCompatActivity {
 
+    ActivityDashboardBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+
+        //binding
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.readableBottomnav.setOnItemSelectListener(new ReadableBottomBar.ItemSelectListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+                //Fragment Transcation
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                switch (i){
+                    case 0:
+                        transaction.replace(R.id.container,new Homefragment());
+                        break;
+                    case 1:
+                        transaction.replace(R.id.container,new SearchFragment());
+                        break;
+                    case 2:
+                        transaction.replace(R.id.container,new AddFragment());
+                        break;
+                    case 3:
+                        transaction.replace(R.id.container,new FavtFragment());
+                        break;
+                    case 4:
+                        transaction.replace(R.id.container,new ProfileFragment());
+                        break;
+                }
+                transaction.commit();
+            }
+        });
     }
 }
